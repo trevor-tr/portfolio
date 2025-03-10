3578d86bbb6565f972e98fe50a400a3f1927825f
trev <trminh.truong@icloud.com>
2025-03-10 22:35:49 +0700

    feat(front): add tailwind and lenis

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import ReactLenis from "lenis/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ReactLenis root>
+      <App />
+    </ReactLenis>
   </StrictMode>
 );
